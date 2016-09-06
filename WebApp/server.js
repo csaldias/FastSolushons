d89ca55f9e76affac2e65d3a8ec0e8b110ad90bd
Camilo Saldías <camilo.saldias@gmail.com>
2016-09-06 12:41:09 -0300

    Maqueta inicio de sesión

---
+++ WebApp/server.js
@@ -0,0 +1,23 @@
+/**
+ * Created by camilo on 06-09-16.
+ */
+//Importar
+
+var express = require('express');
+
+//Instanciar
+var app = express();
+
+//Ruteo
+app.get('/', function (req, res) {
+    res.sendfile(__dirname + '/Views/index.html');
+});
+
+app.get('/login', function (req, res) {
+    res.sendfile(__dirname + '/Views/login.html');
+});
+app.use(express.static('public'));
+
+//Escuchar
+app.listen(9000);
+console.log("Servidor corriendo en http://localhost:9000/");
